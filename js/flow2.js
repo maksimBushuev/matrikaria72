9724b28f9a54ba853c8986cd610034d9678d3a35
maksimBushuev <46831404+maksimBushuev@users.noreply.github.com>
2026-02-24 20:08:15 +0500

    Update flow2.js

--- js/flow2.js
+++ js/flow2.js
@@ -1,3 +1,13 @@
+// w3.getHttpObject("content/flow2.json", function (res) {
+//   w3.displayObject("flow2", res);
+// });
+
 w3.getHttpObject("content/flow2.json", function (res) {
+  // Фильтруем массив внутри объекта res
+  res.flow2 = res.flow2.filter(function (item) {
+    return item.pokaz === "y"; // Оставляем только те, где "y"
+  });
+
+  // Выводим уже отфильтрованный объект
   w3.displayObject("flow2", res);
 });
