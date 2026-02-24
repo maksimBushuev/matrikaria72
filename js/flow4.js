45a12ad38611f523d9fd94a6e396e44e443d7180
maksimBushuev <46831404+maksimBushuev@users.noreply.github.com>
2026-02-24 20:08:51 +0500

    Update flow4.js

--- js/flow4.js
+++ js/flow4.js
@@ -1,3 +1,13 @@
+// w3.getHttpObject("content/flow4.json", function (res) {
+//   w3.displayObject("flow4", res);
+// });
+
 w3.getHttpObject("content/flow4.json", function (res) {
+  // Фильтруем массив внутри объекта res
+  res.flow4 = res.flow4.filter(function (item) {
+    return item.pokaz === "y"; // Оставляем только те, где "y"
+  });
+
+  // Выводим уже отфильтрованный объект
   w3.displayObject("flow4", res);
 });
