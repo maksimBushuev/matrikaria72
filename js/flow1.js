a62d7789be897c9ece6b909f423a88974fac53c0
maksimBushuev <46831404+maksimBushuev@users.noreply.github.com>
2026-02-24 20:07:56 +0500

    Update flow1.js

--- js/flow1.js
+++ js/flow1.js
@@ -1,3 +1,13 @@
+// w3.getHttpObject("content/flow1.json", function (res) {
+//   w3.displayObject("flow1", res);
+// });
+
 w3.getHttpObject("content/flow1.json", function (res) {
+  // Фильтруем массив внутри объекта res
+  res.flow1 = res.flow1.filter(function (item) {
+    return item.pokaz === "y"; // Оставляем только те, где "y"
+  });
+
+  // Выводим уже отфильтрованный объект
   w3.displayObject("flow1", res);
 });
