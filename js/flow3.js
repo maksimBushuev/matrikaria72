2a48f75223e12f5e23ca1f910033e68f34d5e24a
maksimBushuev <46831404+maksimBushuev@users.noreply.github.com>
2026-02-24 20:08:32 +0500

    Update flow3.js

--- js/flow3.js
+++ js/flow3.js
@@ -1,3 +1,13 @@
+// w3.getHttpObject("content/flow3.json", function (res) {
+//   w3.displayObject("flow3", res);
+// });
+
 w3.getHttpObject("content/flow3.json", function (res) {
+  // Фильтруем массив внутри объекта res
+  res.flow3 = res.flow3.filter(function (item) {
+    return item.pokaz === "y"; // Оставляем только те, где "y"
+  });
+
+  // Выводим уже отфильтрованный объект
   w3.displayObject("flow3", res);
 });
